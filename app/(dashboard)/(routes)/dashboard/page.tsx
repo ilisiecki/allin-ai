4aefe647df346bb040b2e0243ba68e5c2fbf62af
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-07-31 20:16:30 +0100

    Add clerk auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
-  return <div className="">Dashboard Page (Protected)</div>;
+  return (
+    <div className="">
+      Dashboard Page (Protected){" "}
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
-  return <div className="">Landing page (Unprotected)</div>;
+  return (
+    <div className="">
+      Landing page (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Sign in</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Sign up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
